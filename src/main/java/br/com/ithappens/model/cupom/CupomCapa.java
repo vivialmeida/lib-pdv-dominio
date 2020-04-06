3c87312f49849f8e5c13ffca8469f9a001aed73b
vivialmeida <viviane.almeida@armateus.com.b>
2020-04-06 16:32:44 -0300

    M - Adicionanco dominio CupomCapa

---
+++ src/main/java/br/com/ithappens/model/cupom/CupomCapa.java
@@ -0,0 +1,51 @@
+package br.com.ithappens.model.cupom;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class CupomCapa {
+        private Long idCupomCapa;
+        private Long idFilial;
+        private Long pdv;
+        private Long cupom;
+        private LocalDateTime dataMovimento;
+        private Long codOperador;
+        private String nomeOperador;
+        private Long situacao;
+        private Integer tipoUso;
+        private Integer operadorMovimento;
+        private BigDecimal valorTotal;
+        private BigDecimal descontoTotal;
+        private BigDecimal _acrescimoTotal;
+        private BigDecimal valorFaturado;
+        private BigDecimal limiteUsado;
+        private BigDecimal troco;
+        private Integer finalizadoraTroco;
+        private Integer quantidadeItens;
+        private Integer quantidadeServicos;
+        private Integer quantidadeFinalizadoras;
+        private String chaveNfe;
+        private Long codVendedor;
+        private Long pedidoFaturado;
+        private String cnpjCliente;
+        private String cpfOperador;
+        private Boolean cancelado;
+        private String chaveNfex;
+        private Boolean enviadoEstoque;
+        private Boolean enviadoFinanceiro;
+        private LocalDateTime dataVenda;
+        private Long cpfCrm;
+        private Boolean enviadoCrm;
+        private  String hash;
+        private Long cupomPai;
+
+
+
+
+
+
+
+
+
+
+}
