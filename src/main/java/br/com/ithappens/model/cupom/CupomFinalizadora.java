1fb3c7df858280ef7e4d263bd22d26a1f44d3610
vivialmeida <viviane.almeida@armateus.com.b>
2020-04-06 17:15:13 -0300

    M - Adicionando cupomFinanlizadoas

---
+++ src/main/java/br/com/ithappens/model/cupom/CupomFinalizadora.java
@@ -0,0 +1,31 @@
+package br.com.ithappens.model.cupom;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class CupomFinalizadora {
+      private Long idCupomFinalizadora;
+      private Long idCupomCapa;
+      private Integer sequencia;
+      private Long finalizadora;
+      private Long bandeira;
+      private BigDecimal valor;
+      private LocalDateTime dataVencimento;
+      private Integer parcelas;
+      private Integer dominio;
+      private Long comp;
+      private Long codigoBanco;
+      private Long agencia;
+      private Long conta;
+      private Long numeroCheque;
+      private String cartao;
+      private Long nsusitef;
+      private Long nsuhost;
+      private String produtoTef;
+      private String codigoAutorizacao;
+      private String nossoNumero;
+      private String cpf_cnpj;
+      private Long cpfProcurador;
+
+
+}
