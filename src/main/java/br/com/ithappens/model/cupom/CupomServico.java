b445fcd75f415c7e4625911ab61997ebc6ed1a8c
vivialmeida <viviane.almeida@armateus.com.b>
2020-04-06 17:23:30 -0300

    M - Adicionando cupomServico

---
+++ src/main/java/br/com/ithappens/model/cupom/CupomServico.java
@@ -0,0 +1,16 @@
+package br.com.ithappens.model.cupom;
+
+import java.math.BigDecimal;
+
+public class CupomServico {
+      private Long idCupomServico;
+      private Long idCupomCapa;
+      private Integer sequencia;
+      private Integer tipoUso;
+      private Long referencia;
+      private BigDecimal valor;
+      private Long documento;
+      private String nsuhost;
+      private String autorizacao;
+
+}
