b35d7e8d59b4aef1e11c9df12779f16c7d28c084
vivialmeida <viviane.almeida@armateus.com.b>
2020-04-07 09:56:16 -0300

    M - adicionando entidade ItemPedido

---
+++ src/main/java/br/com/ithappens/model/pedido/ItemPedido.java
@@ -0,0 +1,52 @@
+package br.com.ithappens.model.pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ItemPedido {
+      private Long idItemPedido;
+      private Long idPedido;
+      private Long idProduto;
+      private BigDecimal quantidade;
+      private char status;
+      private BigDecimal valorTabela;
+      private BigDecimal valorUnitario;
+      private BigDecimal valorAcrescimo;
+      private BigDecimal valorDesconto;
+      private BigDecimal valorCustoMedio;
+      private Long idFornecedor;
+      private Long idDepartamento;
+      private Long idSecao;
+      private BigDecimal valorCustoNota;
+      private BigDecimal valorCustoFinal;
+      private BigDecimal valorCustoMedioContabil;
+      private BigDecimal valorCustoContabil;
+      private BigDecimal valorCustoCompra;
+      private Long idCstIcms;
+      private Long idCstPIS;
+      private Long idCstCofins;
+      private Long idCstIPI;
+      private BigDecimal mva;
+      private BigDecimal reducaoCredito;
+      private BigDecimal reducaoDebito;
+      private Long idCFOP;
+      private BigDecimal valorIPI;
+      private BigDecimal aliquotaIPI;
+      private BigDecimal impostoImportacao;
+      private BigDecimal outrasDespesas;
+      private BigDecimal valorFrete;
+      private BigDecimal valorSeguro;
+      private BigDecimal percentualComissao;
+      private char bonificacao;
+      private String numeroLote;
+      private BigDecimal quantidadeLote;
+      private LocalDate dataFabricacao;
+      private LocalDate dataValidade;
+      private BigDecimal valorJuros;
+      private BigDecimal valorDescontoFiscal;
+      private Integer squencia;
+      private BigDecimal valorTotalItem;
+      private String numeroSerie;
+      private Integer multiploEmbalagem;
+
+}
