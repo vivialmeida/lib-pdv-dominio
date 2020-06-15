af48561917854d173671ec649ee71b73769b0880
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-15 10:43:10 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/cadastro/FornecedorContratoScantech.java
+++ src/main/java/br/com/ithappens/model/cadastro/FornecedorContratoScantech.java
@@ -3,9 +3,10 @@ package br.com.ithappens.model.cadastro;
 import lombok.Data;
 
 @Data
-public class FornecedorContrato {
+public class FornecedorContratoScantech {
 
     private Long id;
     private Long idFornecedor;
     private Integer ativo;
+
 }
