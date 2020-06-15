21574eb2b9bc8da1e4e2ce7a4a3e067df0299ce4
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-15 10:36:32 -0300

    D - Classe fornecedor contrato

---
+++ src/main/java/br/com/ithappens/model/cadastro/FornecedorContrato.java
@@ -0,0 +1,11 @@
+package br.com.ithappens.model.cadastro;
+
+import lombok.Data;
+
+@Data
+public class FornecedorContrato {
+
+    private Long id;
+    private Long idFornecedor;
+    private Integer ativo;
+}
