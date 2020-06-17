fe74609b82253ec97f7aeeb1e9c6847849be1206
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-17 17:18:09 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/cadastro/DTOs/VendaScantechDTO.java
+++ src/main/java/br/com/ithappens/model/cadastro/DTOs/VendaScantechDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +17,7 @@ public class VendaScantechDTO {
     @JsonSerialize(using = LocalDateSerializer.class)
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
-    private LocalDate     fecha;
+    private LocalDateTime fecha;
     private Integer       numero;
     private BigDecimal    descuentoTotal;
     private BigDecimal    recargoTotal;
