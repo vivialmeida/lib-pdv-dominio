c16c9293d3ba71db4c7058cf4c58f69f26891891
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-17 19:40:23 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/cadastro/DTOs/VendaScantechDTO.java
+++ src/main/java/br/com/ithappens/model/cadastro/DTOs/VendaScantechDTO.java
@@ -4,8 +4,8 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZonedDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,23 +13,26 @@ import java.util.List;
 @Data
 public class VendaScantechDTO {
 
-    private String        fecha;
-    private Integer       numero;
-    private BigDecimal    descuentoTotal;
-    private BigDecimal    recargoTotal;
-    private Integer       codigoMoneda = 986;
-    private BigDecimal    cotizacion = BigDecimal.valueOf(1);
-    private BigDecimal    total;
-    private Boolean       cancelacion = false;
-    private String        documentoCliente;
+    private String fecha;
+    private Integer numero;
+    private BigDecimal descuentoTotal;
+    private BigDecimal recargoTotal;
+    private Integer codigoMoneda = 986;
+    private BigDecimal cotizacion = BigDecimal.valueOf(1);
+    private BigDecimal total;
+    private Boolean cancelacion = false;
+    private String documentoCliente;
     private List<DetalheScantechDTO> detalles = new ArrayList<>();
     private List<PagosScantechDTO> pagos = new ArrayList<>();
 
-    public String getFecha(){
+    public String getFecha() {
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime dateTime = LocalDateTime.parse(fecha, formatter);
-        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-        return  ZonedDateTime.from(dateTime).format(formatter);
+
+        OffsetDateTime odt = dateTime.atOffset(ZoneOffset.ofHoursMinutes(1, 0));
+        String s = odt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ"));
+        return s;
+
     }
 }
