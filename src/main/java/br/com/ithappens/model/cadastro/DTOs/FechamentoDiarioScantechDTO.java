85b0484ab1d34763679a2201fa2e414ae6234235
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-18 15:22:52 -0300

    M - correcao

---
+++ src/main/java/br/com/ithappens/model/cadastro/DTOs/FechamentoDiarioScantechDTO.java
@@ -0,0 +1,18 @@
+package br.com.ithappens.model.cadastro.DTOs;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+public class FechamentoDiarioScantechDTO {
+
+    private LocalDate fechaVentas;
+    private BigDecimal montoVentaLiquida;
+    private Integer montoCancelaciones;
+    private Integer cantidadMovimientos;
+    private Integer cantidadCancelaciones;
+
+}
