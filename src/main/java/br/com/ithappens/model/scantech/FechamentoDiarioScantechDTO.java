72303fb806b5515cf05005f854d99ec844b6bfec
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-19 19:00:58 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/scantech/FechamentoDiarioScantechDTO.java
+++ src/main/java/br/com/ithappens/model/scantech/FechamentoDiarioScantechDTO.java
@@ -3,6 +3,7 @@ package br.com.ithappens.model.scantech;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 @Data
 public class FechamentoDiarioScantechDTO {
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaVentas;
     private BigDecimal montoVentaLiquida;
