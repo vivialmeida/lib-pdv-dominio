71aadea91365fb0959194f47974b4d5b454577c9
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-19 09:18:09 -0300

    M - Implementacao de classes scantech

--- src/main/java/br/com/ithappens/model/scantech/FilialScantech.java
+++ src/main/java/br/com/ithappens/model/scantech/FilialScantech.java
@@ -1,9 +1,11 @@
 package br.com.ithappens.model.scantech;
 
 import br.com.ithappens.model.cadastro.DTOs.PdvDTO;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class FilialScantech {
 
     private Long id;
