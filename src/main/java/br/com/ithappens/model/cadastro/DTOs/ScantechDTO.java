95a7883b27af19a22c324e3d422fb306f04d85db
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-18 23:41:29 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/cadastro/DTOs/ScantechDTO.java
+++ src/main/java/br/com/ithappens/model/cadastro/DTOs/ScantechDTO.java
@@ -1,6 +1,9 @@
 package br.com.ithappens.model.cadastro.DTOs;
 
+import br.com.ithappens.model.cadastro.Filial;
 import br.com.ithappens.model.cadastro.FornecedorContratoScantech;
+import br.com.ithappens.model.cadastro.Produto;
+import br.com.ithappens.model.cupom.CupomCapa;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -9,9 +12,12 @@ import java.util.List;
 @Data
 public class ScantechDTO {
 
-    private Long id;
+    private Filial filial;
     private List<Integer> pdv;
+    private List<Integer> movimentos;
     private LocalDate data;
     private List<FornecedorContratoScantech> fornecedor;
+    private List<Produto> produtoList;
+    private List<CupomCapa> cupomCapaList;
 
 }
