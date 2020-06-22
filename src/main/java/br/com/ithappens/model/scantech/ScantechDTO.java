83ba96e1a6aaef98943483f0ec67aa3cd06fb938
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-22 11:54:27 -0300

    M - correcao

--- src/main/java/br/com/ithappens/model/scantech/ScantechDTO.java
+++ src/main/java/br/com/ithappens/model/scantech/ScantechDTO.java
@@ -2,6 +2,7 @@ package br.com.ithappens.model.scantech;
 
 import br.com.ithappens.model.cadastro.DTOs.PdvDTO;
 import br.com.ithappens.model.cadastro.FornecedorContratoScantech;
+import br.com.ithappens.model.tesouraria.LoteSintetico;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -14,6 +15,6 @@ public class ScantechDTO {
     private List<FornecedorContratoScantech> fornecedor;
     private FechamentoDiarioScantechDTO fechamento;
     private Long idFilial;
-    private Long idLoteSintetico;
+    private LoteSintetico loteSintetico;
     private List<PdvDTO> pdvs;
 }
