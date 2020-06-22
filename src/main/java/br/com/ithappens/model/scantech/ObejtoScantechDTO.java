bd7b4d83784984be909a80d6fe8a288c5f217dc1
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-06-21 21:12:38 -0300

    M - atulaizar

---
+++ src/main/java/br/com/ithappens/model/scantech/ObejtoScantechDTO.java
@@ -0,0 +1,17 @@
+package br.com.ithappens.model.scantech;
+
+import br.com.ithappens.model.cadastro.Produto;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class ObejtoScantechDTO {
+
+    private Long idFilial;
+    private Integer pdv;
+    private List<Produto> prodList;
+    private LocalDate data;
+    private List<VendaScantechDTO> cupons;
+}
