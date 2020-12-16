a8838fd8cdf576c585b90cec8e2a4f11cfd0897b
Gabrielle Limach <gabrielle.lmm@gmail.com>
2020-12-16 10:55:11 -0300

    M -correcao

--- src/main/java/br/com/ithappens/model/cadastro/AgenteCobrador.java
+++ src/main/java/br/com/ithappens/model/cadastro/AgenteCobrador.java
@@ -1,11 +1,9 @@
 package br.com.ithappens.model.cadastro;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
--- src/main/java/br/com/ithappens/model/cadastro/Empresa.java
+++ src/main/java/br/com/ithappens/model/cadastro/Empresa.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.beans.ConstructorProperties;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
